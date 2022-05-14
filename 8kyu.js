d21e5d3bce45b992d4a4641c0ff424a418adb6c2
otoronis <91470128+otoronis@users.noreply.github.com>
2022-05-14 12:43:02 +0100

    51

--- 8kyu.js
+++ 8kyu.js
@@ -10,3 +10,6 @@
 
 // 50. Will You Make It?
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= fuelLeft * mpg
+
+// 51. Simple Multiplication
+const simpleMultiplication = number => (number % 2 === 0) ? number * 8 : number * 9

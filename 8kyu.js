f0c7df369570d6753b23fe1190dca40dcc30e0c1
otoronis <91470128+otoronis@users.noreply.github.com>
2022-05-14 12:47:35 +0100

    52

--- 8kyu.js
+++ 8kyu.js
@@ -8,8 +8,16 @@
 
 // ...
 
-// 50. Will You Make It?
+// 50. Will you make it?
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= fuelLeft * mpg
 
-// 51. Simple Multiplication
+// 51. Simple multiplication
 const simpleMultiplication = number => (number % 2 === 0) ? number * 8 : number * 9
+
+// 52. How good are you really?
+function betterThanAverage(classPoints, yourPoints) {
+  classPoints.push(yourPoints);
+  let sum = classPoints.reduce((sum, x) => sum + x, 0);
+  const average = Math.round(sum / classPoints.length);
+  return yourPoints > average
+}

fd896f0ec85986824d4a5796056077522f7a8006
otoronis <91470128+otoronis@users.noreply.github.com>
2022-05-14 12:40:55 +0100

    Create 8kyu.js

---
+++ 8kyu.js
@@ -0,0 +1,12 @@
+// My answers to 8kyu problems
+
+// 1. Odd or Even
+
+// 2. Opposite Numbers
+
+// 3. L1 Set Alarm
+
+// ...
+
+// 50. Will You Make It?
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= fuelLeft * mpg
